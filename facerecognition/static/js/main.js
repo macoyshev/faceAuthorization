a88794d98eaac2d68d6ff34d56283e57a7aed20c
macoyshev <make.oynoshev@gmail.com>
2021-05-23 12:31:32 +0700

    'webcam

--- facerecognition/static/js/main.js
+++ facerecognition/static/js/main.js
@@ -1,32 +1,32 @@
 const video = document.getElementById('value')
 
-// let cam
-//
-// function setup() {
-//
-//   const form = document.getElementById('photoForm')
-//   const button = document.getElementById('enter')
-//
-//   cam = createCapture(VIDEO)
-//
-//   button.addEventListener('click', ()=>{
-//     cam.loadPixels()
-//     let data = cam.canvas.toDataURL("image/png")
-//     data = data.replace(/^data:image\/(png|jpg);base64,/, "");
-//     form.value = data
-//   })
-//
-//   noCanvas();
-// }
+let cam
 
-function startapp(){
-  navigator.mediaDevices.getUserMedia({
-    video: true,
-    audio: false,
-  }).then(stream => {
-    video.srcObject = stream
+function setup() {
+
+  const form = document.getElementById('photoForm')
+  const button = document.getElementById('enter')
+
+  cam = createCapture(VIDEO)
+
+  button.addEventListener('click', ()=>{
+    cam.loadPixels()
+    let data = cam.canvas.toDataURL("image/png")
+    data = data.replace(/^data:image\/(png|jpg);base64,/, "");
+    form.value = data
   })
 
+  noCanvas();
 }
 
-window.addEventListener('load', startapp, false)
+// function startapp(){
+//   navigator.mediaDevices.getUserMedia({
+//     video: true,
+//     audio: false,
+//   }).then(stream => {
+//     video.srcObject = stream
+//   })
+//
+// }
+//
+// window.addEventListener('load', startapp, false)
